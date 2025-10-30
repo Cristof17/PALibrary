c98f9c9270c4d84be38a8a72ee6b5a2645cf8656
Cristof17 <cristof_rotsching@yahoo.com>
2025-10-30 09:31:49 +0200

    solved errors in programming in PALista.h

--- PALista.h
+++ PALista.h
@@ -4,10 +4,10 @@ struct PALista
 	struct PANod head;
 };
 
-int size(paLista lista)
-paNod get(int position)
+int size(paLista lista);
+paNod get(int position);
 paNod head(paLista lista);
 paLista tail(paLista lista)
-paNod init(paLista)
+paNod init(paLista);
 //toArrayList();
 
