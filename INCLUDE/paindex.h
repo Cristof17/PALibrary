630c9cf56fe151f24077fc5867c91aec2e9b5b44
Cristof17 <cristof_rotsching@yahoo.com>
2025-11-16 16:35:23 +0200

    INT instead if int iun paindex.h

--- INCLUDE/paindex.h
+++ INCLUDE/paindex.h
@@ -5,10 +5,10 @@ struct PAIndex
     int Value;
 };
 typedef struct PAIndex PAIndex;
-int op_Equality(PAIndex index, PAIndex other);
-int op_Inequality(PAIndex index, PAIndex other);
-int op_LessThan(PAIndex index, PAIndex other);
-int op_GreatherThan(PAIndex index, PAIndex other);
-int op_LessThan(PAIndex index, PAIndex other);
-int op_GreatherThanOrEqual(PAIndex index, PAIndex other);
+INT op_Equality(PAIndex index, PAIndex other);
+INT op_Inequality(PAIndex index, PAIndex other);
+INT op_LessThan(PAIndex index, PAIndex other);
+INT op_GreatherThan(PAIndex index, PAIndex other);
+INT op_LessThan(PAIndex index, PAIndex other);
+INT op_GreatherThanOrEqual(PAIndex index, PAIndex other);
 #endif
