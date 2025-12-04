4d8dc251563fe49dcc9c9ab1ffbf98c8edcf682e
Cristof17 <Cristof17>
2025-12-05 01:15:23 +0200

    updated defs.h with include guard for defs.h

--- include/defs.h
+++ include/defs.h
@@ -1,53 +1,35 @@
-
+#ifndef INCLUDE_DEFS
+#define INCLUDE_DEFS
+#include <types.h>
 struct Input {
 	;
 };
 struct Algorithm {
 	struct Input input;
 };
-struct BFSInput {
-	struct PACardinal n;
-	struct PACardinal m;
-	struct PALista adj;
-	struct PANod sursa;
-};
-struct BFSOutput {
-    struct BFSResult Result;
-};
-struct BFSResult {
-	struct PALista Lista;
-	struct PACardinal Cardinal;
-};
-struct Output {
-//	struct PARezultat rezultat;
-//	struct BFSOutput outputBFS;
-};
 struct PACardinal {
 
-};
-struct PAData {
-    INT value;
-};
-struct PAGraf  {
-	struct PACardinal n;
-	struct PACardinal m;
-	struct PANod sursa;
-	struct PASir adj;
 };
 struct PAIndex {
 //    int Value;
 	INT Value;
+};
+struct ArrayList {
+	struct PAIndex Index[0];
 };
 struct PALista {
 	// struct PANod Nod;
 	struct ArrayList List;
 	struct PACardinal m;
 };
-struct PAMuchie {
-	struct PAPereche p;
-};
 struct PANeigh {
     //struct PANod nod;
+};
+struct PAStare {
+	INT vizitat;
+};
+struct PATip {
+	INT Kind;
 };
 struct PANod { 
 //	struct PAData data;
@@ -56,20 +38,43 @@ struct PANod {
 	struct PAStare Stare;
 	struct PATip Tip;
 };
-struct PAPereche {
+struct BFSInput {
+	struct PACardinal n;
+	struct PACardinal m;
+	struct PALista adj;
+	struct PANod sursa;
 };
-struct PARezultat {
-	INT cod;
+struct BFSResult {
+	struct PALista Lista;
+	struct PACardinal Cardinal;
+};
+struct BFSOutput {
+    struct BFSResult Result;
+};
+struct Output {
+//	struct PARezultat rezultat;
+//	struct BFSOutput outputBFS;
+};
+struct PAData {
+    INT value;
 };
 struct PASir {
 	struct PACardinal n;
 	struct PALista adj[];
 };
-struct PAStare {
-	INT vizitat;
+struct PAGraf  {
+	struct PACardinal n;
+	struct PACardinal m;
+	struct PANod sursa;
+	struct PASir adj;
 };
-struct PATip {
-	INT Kind;
+struct PAPereche {
+};
+struct PAMuchie {
+	struct PAPereche p;
+};
+struct PARezultat {
+	INT cod;
 };
 struct Adaptee {
 	struct ArrayList List;
@@ -77,47 +82,45 @@ struct Adaptee {
 struct Adapter {
 	struct PALista Lista;
 };
-struct Client {
+struct IteratorClient {
 	struct PAGraf Graf;
 };
 struct Target {
 	struct PASir Sir;
 	//struct Adapter adapter;
 };
-struct ArrayList {
-	struct PAIndex Index[0];
-};
-struct Builder {
 
+struct Builder {
+    ;
 };
 struct Director {
     struct Builder builder;
 };
 struct GrafNormal {
-
+    ;
 };
 struct GrafProduct {
     struct PAGraf graf;
 };
 struct GrafTranspus {
-
+    ;
 };
 struct Aggregate {
 };
-struct Client {
-
+struct BuilderClient {
+    ;
 };
 struct ConcreteAggregate {
-
+    ;
 };
 struct ConcreteIterator  {
-
+    ;
 };
-
 struct Iterator  {
-
+    
 };
 struct BFSAlgorithm {
 	struct BFSInput Input;
 	struct PAGraf Graf;
 };
+#endif
