9111ed59e7e7865f2a9520ea5302e8c50e4a60fe
Cristof17 <Cristof17>
2025-12-04 21:52:14 +0200

    solved include error in Adaptee

--- include/Adapter/adaptee.h
+++ include/Adapter/adaptee.h
@@ -9,7 +9,7 @@
 #define INCLUDE_ADAPTEE_H_
 //#include <adaptee.h>
 #include "include/PAIndex.h"
-#include "include/ArrayList.h"
+#include "include/ArrayList/ArrayList.h"
 #include <types.h>
 
 struct Adaptee
