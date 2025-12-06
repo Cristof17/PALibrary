713e4a7ee12b5861c69dcec7d40eaf8e9be0b3c5
Cristof17 <Cristof17>
2025-12-06 12:37:58 +0200

    renamed PAItem to PAData

--- src/PAData.c
+++ src/PAData.c
@@ -1,3 +1,17 @@
-//@Author Cristofor Rotsching
-#include "../include/PAData.h"
+#include "../include/PAIndex.h"
 #include <defs.h>
+INT op_Equality(struct PAIndex,struct PAIndex) {
+    return 1;
+}
+INT op_Inequality(struct PAIndex,struct PAIndex) {
+    return 1;
+}
+INT op_GreatherThan(struct PAIndex,struct PAIndex) {
+    return 1;
+}
+INT op_LessThan(struct PAIndex,struct PAIndex) {
+    return 1;
+}
+INT op_GreatherThanOrEqual(struct PAIndex,struct PAIndex) {
+    return 1;
+}
