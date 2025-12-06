16e9a5df95d7698b095d2efbed6603a9b9084e7c
Cristof17 <Cristof17>
2025-12-06 15:01:13 +0200

    PAData replaces PAindex in PAVAlue

--- src/PAValue.c
+++ src/PAValue.c
@@ -1,17 +1,17 @@
 #include "../include/PAData.h"
 #include <defs.h>
-INT op_Equality(struct PAIndex,struct PAIndex) {
+INT op_Equality(struct PAData,struct PAData) {
     return 1;
 }
-INT op_Inequality(struct PAIndex,struct PAIndex) {
+INT op_Inequality(struct PAData,struct PAData) {
     return 1;
 }
-INT op_GreatherThan(struct PAIndex,struct PAIndex) {
+INT op_GreatherThan(struct PAData,struct PAData) {
     return 1;
 }
-INT op_LessThan(struct PAIndex,struct PAIndex) {
+INT op_LessThan(struct PAData,struct PAData) {
     return 1;
 }
-INT op_GreatherThanOrEqual(struct PAIndex,struct PAIndex) {
+INT op_GreatherThanOrEqual(struct PAData,struct PAData) {
     return 1;
 }
