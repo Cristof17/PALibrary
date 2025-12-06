25734651f2105de3d5bf4fa19899c820bf28b384
Cristof17 <Cristof17>
2025-12-06 14:34:45 +0200

    renamed Cardinal to Count

--- include/PACount.h
+++ include/PACount.h
@@ -1,7 +1,7 @@
 //@Author Cristofor Rotsching
 #ifndef INCLUDE_PACARDINAL_H
 #define INCLUDE_PACARDINAL_H
-struct PACardinal;
-typedef struct PACardinal PACardinal;
+struct PACount;
+typedef struct PACount PACount;
 void PACardinalPrint(struct PACardinal);
 #endif
