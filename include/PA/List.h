239d160cb290495e2d1a4eb435811d2987753081
Cristof17 <cristof_rotsching@yahoo.com>
2025-12-08 19:55:22 +0200

    updated Graph Diagram include and source files

--- include/PA/List.h
+++ include/PA/List.h
@@ -7,9 +7,15 @@
 struct PAList;
 typedef struct PAList List;
 
+void PAList(struct PAElement Head);
+void PAListDestroy();
+struct PAList PAListInit(struct PAList);
+//s
+void Dispose();
 INT PAListSize(struct PAList);
-struct PAElement PAListGet(INT);
-struct PAElement PAListHead(struct PAList);
+//struct PAElement PAListGet(INT);
+struct PAElement PAListGetHead(struct PAList);
+void PAListAddHead(struct PAElement);
 struct PAList PAListTail(struct PAList);
 struct PAElement PAListInit(struct PAList);
 void PAListPrint(struct PAList);
