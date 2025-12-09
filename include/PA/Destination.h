6eac5f1a25b2de06e535d665e9f25f7d85627b5e
Cristof17 <cristof_rotsching@yahoo.com>
2025-12-09 18:33:41 +0200

    circular dependency silved

--- include/PA/Destination.h
+++ include/PA/Destination.h
@@ -1,11 +1,11 @@
 //@Author Cristofor Rotsching
 #ifndef INCLUDE_PA_DESTINATION_H
 #define INCLUDE_PA_DESTINATION_H
-#include <PA/Element.h>
+//#include <PA/Element.h>
 struct PADestination;
 struct PADestination PADestinationConstruct();
 void PADestinationRuin();
-struct PADestination PADataArrange(struct PADestination);
-struct PADestination PADataPutElement(struct PADestination,struct PAElement);
+struct PADestination PADestinationArrange(struct PADestination);
+//struct PADestination PADestinationPutElement(struct PADestination,struct PAElement element);
 //typedef struct PADestination Destination;
 #endif
