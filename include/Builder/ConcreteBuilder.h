f0be1928ddb41ee1006b49e9d280d6a8bd65f404
Cristof17 <cristof_rotsching@yahoo.com>
2025-12-14 13:34:27 +0200

    created ConcreteBuilder.h

---
+++ include/Builder/ConcreteBuilder.h
@@ -0,0 +1,8 @@
+#ifndef CONCRETEBUILDER_H_
+#define CONCRETEBUILDER_H_
+#ifndef _WIN95
+#elif defined _WIN95
+#endif
+struct ConcreteBuilder {
+
+};
---
+++ src/Builder/ConcreteBuilder.c
@@ -0,0 +1,9 @@
+/*
+ * ConcreteBuilder.c
+ *
+ *  Created on: 14 dec. 2025
+ *      Author: AdministratorUser
+ */
+#include <Builder/ConcreteBuilder.h>
+
+
