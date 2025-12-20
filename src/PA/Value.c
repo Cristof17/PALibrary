58decefa0c5bb4efe9cc67781feda0d55aa97d9b
Cristof17 <Cristof17>
2025-12-20 10:34:09 +0200

    updated Value.h

--- src/PA/Value.c
+++ src/PA/Value.c
@@ -10,11 +10,14 @@ struct PAResult PAValueRuin(struct PAValue Value) {
 }
 struct PAValue PAValueArrange(struct PAValue Value) {
     struct PAValue value;
+    value.value = Value.value;
     return value;
 }
 struct PAValue PAValuePutValue(struct PAValue Value1, PA_INT Value2) {
-    struct PAValue value;
-    return value;
+    // struct PAValue value;
+    Value1.value = Value2;
+    // return value;
+    return Value1;
 }
 //typedef struct PAValue Value;
 PA_INT op_Equality(struct PAValue Value1,struct PAValue Value2) {
