ac44281046e86c7d9d1a5a132642d67e3c50260b
Cristof17 <Cristof17>
2025-12-23 12:45:34 +0200

    Series Status Tree Value Pair NormalTree List Link Feature Element Destinatino Data Count header and C source files updated to have Construct Init Ruin Delete and Copy methods

--- src/PA/Value.c
+++ src/PA/Value.c
@@ -1,22 +1,26 @@
 #include <PA/Value.h>
 #include <defs.h>
-struct PAValue PAValuePerformConstruct() 
+struct PAValue PAValuePerformConstruct()
 {
     struct PAValue value;
     return value;
 }
-struct PAResult PAValuePerformRuin(struct PAValue Value) 
+struct PAResult PAValuePerformRuin(struct PAValue)
 {
     struct PAResult result;
     return result;
 }
-struct PAValue PAValuePerformInit(struct PAValue Value) 
+struct PAValue PAValuePerformCopy(struct PAValue)
+{
+
+}
+struct PAValue PAValuePerformInit(PA_INT)
 {
     struct PAValue value;
     value.value = Value.value;
     return value;
 }
-struct PAValue PAValuePerformPutValue(struct PAValue Value1, PA_INT Value2) 
+struct PAValue PAValuePerformPutValue(struct PAValue, PA_INT)
 {
     // struct PAValue value;
     Value1.value = Value2;
@@ -24,23 +28,23 @@ struct PAValue PAValuePerformPutValue(struct PAValue Value1, PA_INT Value2)
     return Value1;
 }
 //typedef struct PAValue Value;
-PA_INT op_Equality(struct PAValue Value1,struct PAValue Value2) 
+PA_INT op_Equality(struct PAValue,struct PAValue)
 {
     return 1;
 }
-PA_INT op_Inequality(struct PAValue Value1,struct PAValue Value2) 
+PA_INT op_Inequality(struct PAValue,struct PAValue)
 {
     return 1;
 }
-PA_INT op_LessThan(struct PAValue Value1,struct PAValue Value2) 
+PA_INT op_LessThan(struct PAValue,struct PAValue)
 {
     return 1;
 }
-PA_INT op_GreatherThan(struct PAValue Value1,struct PAValue Value2) 
+PA_INT op_GreatherThan(struct PAValue,struct PAValue)
 {
     return 1;
 }
-PA_INT op_GreatherThanOrEqual(struct PAValue PAValue1,struct PAValue Value2) 
+PA_INT op_GreatherThanOrEqual(struct PAValue,struct PAValue)
 {
     return 1;
 }
