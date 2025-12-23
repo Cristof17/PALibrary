b6421e86f1260d4d7ac8395191b8a9f39c0ce9fd
Cristof17 <Cristof17>
2025-12-23 13:31:25 +0200

    PAStatus updated

--- src/PA/Status.c
+++ src/PA/Status.c
@@ -6,25 +6,25 @@ struct PAStatus PAStatusPerformConstruct()
     struct PAStatus status;
     return status;
 }
-struct PAResult PAStatusPerformRuin(struct PAStatus)
+struct PAResult PAStatusPerformRuin(struct PAStatus Status)
 {
     struct PAResult result;
     return result;
 }
-struct PAStatus PAStatusPerformCopy(struct PAStatus)
+struct PAStatus PAStatusPerformCopy(struct PAStatus Status)
 {
     struct PAStatus status;
     status.visited = Status.visited;
     return status;
 }
-struct PAStatus PAStatusPerformInit(PA_INT)
+struct PAStatus PAStatusPerformInit(PA_INT Visited)
 {
     // struct PAStatus status;
     Status.visited = Value;
     // return status;
     return Status;
 }
-struct PAStatus PAStatusPerformPutValue(struct PAStatus, PA_INT)
+struct PAStatus PAStatusPerformPutValue(struct PAStatus Status, PA_INT Visited)
 {
 
 }
