0892555f98c2c44ef3daebefd4d52bfe41b5ea8f
Cristof17 <Cristof17>
2025-12-24 21:52:28 +0200

    ArrayList function signatire

--- src/ArrayList/ArrayList.c
+++ src/ArrayList/ArrayList.c
@@ -7,30 +7,30 @@
  *  Created on: 20 nov. 2025
  *      Author: AdministratorUser
  */
-struct ArrayList ArrayListConstruct()
+struct ArrayList ArrayListPeformConstruct()
 {
 	struct ArrayList List;
 	return List;
 }
-struct ArrayList ArrayListInit(struct PAData Data[], struct PACount M)
+struct ArrayList ArrayListPerformInit(struct PAData Data[], struct PACount M)
 {
 	// srt
 	struct ArrayList List;
-	List = ArrayListConstruct();
+	List = ArrayListPerformConstruct();
 	List.index = PADataPerformCopy(Data);
 	return List;
 }
-struct ArrayList ArrayListCopy(struct ArrayList List)
+struct ArrayList ArrayListPerformCopy(struct ArrayList List)
 {
 	struct ArrayList Lost;
 	return Lost;
 }
-struct PAData ArrayListGet(struct PAIndex Array[],PAInt Position) {
+struct PAData ArrayListPerformGet(struct PAIndex Array[],PAInt Position) {
 	struct PAData index;
 	// = Array[Position];
 	return index;
 }
-PAResult Init(PAInt size) {
+PAResult ArrayListPeformInit(PAInt size) {
 //	return )
 	PAResult rezultat;
 	return rezultat;
