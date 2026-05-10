651d137e10f607d115ea9708f50d878922fae813
Cristof17 <Cristof17>
2026-05-11 00:34:10 +0300

    PATree.h

--- include/PA/Value.h
+++ include/PA/Value.h
@@ -6,16 +6,16 @@
 #include <defs.h>
 
 // struct PAValue;
-DllExport struct PAValue PAValuePerformConstruct(PAInt);
-DllExport struct PAValue PAValuePerformCopy(struct PAValue);
-DllExport struct PAValue PAValuePerformInit(struct PAValue);
-DllExport struct PAValue PAValuePerformPutValue(struct PAValue, PAInt);
-DllExport HRESULT PAValuePerformRuin(struct PAValue);
+// DllExport struct PAValue PAValuePerformConstruct(PAInt);
+// DllExport struct PAValue PAValuePerformCopy(struct PAValue);
+// DllExport struct PAValue PAValuePerformInit(struct PAValue);
+// DllExport struct PAValue PAValuePerformPutValue(struct PAValue, PAInt);
+// DllExport HRESULT PAValuePerformRuin(struct PAValue);
 // struct PAResult PAValueDelete(stru)
 //typedef struct PAValue Value;
-DllExport PABool op_Equality(struct PAValue,struct PAValue);
-DllExport PABool op_Inequality(struct PAValue,struct PAValue);
-DllExport PABool op_LessThan(struct PAValue,struct PAValue);
-DllExport PABool op_GreatherThan(struct PAValue,struct PAValue);
-DllExport PABool op_GreatherThanOrEqual(struct PAValue,struct PAValue);
+// DllExport PABool op_Equality(struct PAValue,struct PAValue);
+// DllExport PABool op_Inequality(struct PAValue,struct PAValue);
+// DllExport PABool op_LessThan(struct PAValue,struct PAValue);
+// DllExport PABool op_GreatherThan(struct PAValue,struct PAValue);
+// DllExport PABool op_GreatherThanOrEqual(struct PAValue,struct PAValue);
 #endif
