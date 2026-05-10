34c4fe94eb469b24302642ef47332274c14d9875
Cristof17 <Cristof17>
2026-05-10 23:31:22 +0300

    updated Data.h

--- include/PA/Data.h
+++ include/PA/Data.h
@@ -8,11 +8,11 @@
 #include <defs.h>
 // #include <defs.h>
 // struct PAData;
-DllExport struct PAData PADataPerformConstruct(PAResource Resource);
+DllExport struct PAData PADataPerformConstruct(PAInt Resource);
 DllExport struct PAData PADataPerformInit(struct PAData);
-DllExport struct PAData PADataPerformCopy(struct PAData);
-DllExport struct PAData PADataPerformPutResource(struct PAData, PAResource);
-DllExport HRESULT PADataPerformRuin(struct PAData);
-DllExport HRESULT PADataPerformDelete(struct PAData);
+// DllExport struct PAData PADataPerformCopy(struct PAData);
+// DllExport struct PAData PADataPerformPutResource(struct PAData, PAResource);
+DllExport int PADataPerformRuin(PAInt Resource);
+DllExport int PADataPerformDelete(struct PAData PA);
 //typedef struct PAData Data;
 #endif
