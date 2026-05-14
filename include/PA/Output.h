4eb3b081fea786b7909742509da7ddb766f6d1a1
Cristof17 <Cristof17>
2026-05-14 08:50:10 +0300

    updated Output.h and Output.c

--- include/PA/Output.h
+++ include/PA/Output.h
@@ -10,10 +10,10 @@
 // struct PAData PADataPerformConstruct(PAInt Resource);
 // struct PAData PADataPerformCopy(struct PAData Data);
 // strut BFSOutputRuin();
-struct BFSRecord OutputPerformConstruct(struct BFSRecord result);
-struct Output PADataPerformInit(struct BFSRecord record);
+DllExport struct BFSRecord OutputPerformConstruct(struct BFSRecord result);
+DllExport struct Output PADataPerformInit(struct BFSRecord record);
 //typedef struct BFSOutput BFSOutput;
 // struct PAOutput PAOutputPerformInit(struct BFSRecord);
-int PADataPerformRuin(struct BFSRecord record);
-int PADataPerformDelete(struct BFSRecord bfs);
+DllExport int PADataPerformRuin(struct BFSRecord record);
+DllExport int PADataPerformDelete(struct BFSRecord bfs);
 #endif
