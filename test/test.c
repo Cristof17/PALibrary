52d9897355857bb5bd8263ae3ab91a86c2b64741
Cristof17 <Cristof17>
2026-05-18 18:06:15 +0300

    Data and Element construct working

--- test/test.c
+++ test/test.c
@@ -1,4 +1,9 @@
 #include <PA/Tree.h>
+#include <PA/Count.h>
+#include <types.h>
+#include <PA/Element.h>
+#include <PA/Data.h>
+#include <PA/Element.h>
 #include <stdio.h>
 int main()
 {
@@ -6,6 +11,13 @@ int main()
 	struct PATree tree2 = PATreePerformConstruct();
 	printf("testing tree %d\n", tree.n.number.val);
 	printf("testing copy %d\n", tree2.n.number.val);
+	struct PACount count1 = PACountPerformConstruct();
+	printf("testing count %d\n", count1.number.val);
+	struct PAData Data;
+	Data = PADataPerformConstruct();
+	printf("testing Data %d\n",Data.Resource.value.val);
+	struct PAElement element = PAElementPerformConstruct();
+	// printf("testing element %d,%d,%d\n",element.status.)
 	// PATreePerformCopy(tree,tree2);
 	return 0;
 }
---
+++ test/test.s
@@ -0,0 +1,106 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 15, 0	sdk_version 26, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #112
+	stp	x29, x30, [sp, #96]             ; 16-byte Folded Spill
+	add	x29, sp, #96
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	stur	x8, [x29, #-8]
+	stur	wzr, [x29, #-44]
+	bl	_PATreePerformConstruct
+	sub	x8, x29, #24
+	stur	x0, [x29, #-24]
+	stur	x1, [x29, #-16]
+	ldur	x10, [x29, #-24]
+	add	x9, sp, #32
+	str	x10, [sp, #32]
+	ldur	x8, [x8, #7]
+	stur	x8, [x9, #7]
+	bl	_PATreePerformConstruct
+	sub	x8, x29, #40
+	stur	x0, [x29, #-40]
+	stur	x1, [x29, #-32]
+	ldur	x10, [x29, #-40]
+	add	x9, sp, #16
+	str	x10, [sp, #16]
+	ldur	x8, [x8, #7]
+	stur	x8, [x9, #7]
+	ldrb	w10, [sp, #32]
+	mov	x9, sp
+                                        ; implicit-def: $x8
+	mov	x8, x10
+	str	x8, [x9]
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	ldrb	w10, [sp, #16]
+	mov	x9, sp
+                                        ; implicit-def: $x8
+	mov	x8, x10
+	str	x8, [x9]
+	adrp	x0, l_.str.1@PAGE
+	add	x0, x0, l_.str.1@PAGEOFF
+	bl	_printf
+	bl	_PACountPerformConstruct
+	strb	w0, [sp, #15]
+	ldrb	w10, [sp, #15]
+	mov	x9, sp
+                                        ; implicit-def: $x8
+	mov	x8, x10
+	str	x8, [x9]
+	adrp	x0, l_.str.2@PAGE
+	add	x0, x0, l_.str.2@PAGEOFF
+	bl	_printf
+	bl	_PADataPerformConstruct
+	strb	w0, [sp, #13]
+	ldrb	w8, [sp, #13]
+	strb	w8, [sp, #14]
+	ldrb	w10, [sp, #14]
+	mov	x9, sp
+                                        ; implicit-def: $x8
+	mov	x8, x10
+	str	x8, [x9]
+	adrp	x0, l_.str.3@PAGE
+	add	x0, x0, l_.str.3@PAGEOFF
+	bl	_printf
+	bl	_PAElementPerformConstruct
+	sturh	w0, [sp, #11]
+	ldur	x9, [x29, #-8]
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	subs	x8, x8, x9
+	b.eq	LBB0_2
+	b	LBB0_1
+LBB0_1:
+	bl	___stack_chk_fail
+LBB0_2:
+	mov	w0, #0                          ; =0x0
+	ldp	x29, x30, [sp, #96]             ; 16-byte Folded Reload
+	add	sp, sp, #112
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"testing tree %d\n"
+
+l_.str.1:                               ; @.str.1
+	.asciz	"testing copy %d\n"
+
+l_.str.2:                               ; @.str.2
+	.asciz	"testing count %d\n"
+
+l_.str.3:                               ; @.str.3
+	.asciz	"testing Data %d\n"
+
+.subsections_via_symbols
