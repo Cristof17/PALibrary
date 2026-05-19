a6f9f2e7480aca81bf491b3068dcab5bfa2f01b5
Cristof17 <Cristof17>
2026-05-19 09:06:00 +0300

    Abort at PATreePerformConstruct on arm64

--- test/test.c
+++ test/test.c
@@ -11,19 +11,20 @@ int main()
 {
 	struct PANumber number;
 	number = PANumberPerformConstruct();
+	printf("testing PANumber %d\n()",number.val);
 	struct PAResource resource;
 	resource = PAResourcePerformConstruct();
 	printf("testing Resource %d\n",resource.value.val);
 	struct PATree tree = PATreePerformConstruct();
-	struct PATree tree2 = PATreePerformConstruct();
-	printf("testing tree %d\n", tree.n.number.val);
-	printf("testing copy %d\n", tree2.n.number.val);
-	struct PACount count1 = PACountPerformConstruct();
-	printf("testing count %d\n", count1.number.val);
-	struct PAData Data;
-	Data = PADataPerformConstruct();
-	printf("testing Data %d\n",Data.Resource.value.val);
-	struct PAElement element = PAElementPerformConstruct();
+	///struct PATree tree2 = PATreePerformConstruct();
+	//printf("testing tree %d\n", tree.n.number.val);
+	//printf("testing copy %d\n", tree2.n.number.val);
+	//struct PACount count1 = PACountPerformConstruct();
+	//printf("testing count %d\n", count1.number.val);
+	//struct PAData Data;
+	//Data = PADataPerformConstruct();
+	//printf("testing Data %d\n",Data.Resource.value.val);
+	//struct PAElement element = PAElementPerformConstruct();
 
 	// printf("testing element %d,%d,%d\n",element.status.)
 	// PATreePerformCopy(tree,tree2);
