533428acb53240e14e69d820e9ee8e8ef9906c21
Cristof17 <Cristof17>
2026-06-03 09:17:59 +0300

    Link.h using poinnters

--- include/PA/Link.h
+++ include/PA/Link.h
@@ -8,9 +8,9 @@
 DllExport struct PALink* PALinkPerformConstruct();
 DllExport struct PALink* PALinkPerformCopy(struct PALink*, struct PALink*);
 // DllExport struct PALink PALinkPerformCopy(struct PALink);
-DllExport struct PALink PALinkPerformInit(struct PALink*, struct PAPair);
+DllExport struct PALink* PALinkPerformInit(struct PALink*, struct PAPair);
 // DllExport struct PALink PALinkPerformPutPair(struct PALink,struct PAPair);
-DllExport struct PALink PALinkPerformRuin(struct PALink*);
-DllExport struct PALink PALinkPerformDelete(struct PALink*);
+DllExport struct PALink* PALinkPerformRuin(struct PALink*);
+DllExport struct PALink* PALinkPerformDelete(struct PALink*);
 //typedef struct PALink Link;
 #endif
