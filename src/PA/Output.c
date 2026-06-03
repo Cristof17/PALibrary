f0436c1a1b8df293233c111e23466c50048c6750
Cristof17 <Cristof17>
2026-06-03 08:44:19 +0300

    Count pointers functions

--- src/PA/Output.c
+++ src/PA/Output.c
@@ -73,7 +73,7 @@ DllExport struct PAOutput* PAOutputPerformDelete(struct PAOutput* Output)
     // struct PAOutput Output;
     return Output;
 }
-DllExport struct PAOutput PAOutputPerformRuin(struct PAOutput* PA)
+DllExport struct PAOutput* PAOutputPerformRuin(struct PAOutput* PA)
 {
 
     // PA.result = BFSRecordPerformRuin
