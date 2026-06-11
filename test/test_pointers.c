4134f7566f524f90ba0436bc263eb90b1a4032d1
Cristof17 <Cristof17>
2026-06-11 12:35:00 +0300

    Count.h CountBegin instead of CompleteBegin

--- test/test_pointers.c
+++ test/test_pointers.c
@@ -1,4 +1,17 @@
+#include <PA/Count.h>
+#include <PA/Resource.h>
+#include <PA/Number.h>
 int main()
 {
+	struct PANumber* number;
+	number=PANumberCreate();
+	PANumberBegin(number,'a');
+	struct PAResource* resource;
+	resource=PAResourceCreate();
+	PAResourceBegin(resource,*number);
+	struct PACount* count;
+	count = PACountCreate();
+	PACountBegin(count,*resource);
+	PACountPrint(count);
 	return 0;
 }
