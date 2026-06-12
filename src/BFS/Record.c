8bb9482ea0f43905757244990062fe3e7b0749da
Cristof17 <Cristof17>
2026-06-12 13:04:18 +0300

    BFSRecord

--- src/BFS/Record.c
+++ src/BFS/Record.c
@@ -15,8 +15,32 @@
 #endif
 // struct PALista Lista;
 	// struct PACardinal Cardinal;
+DllExport struct BFSRecord* BFSRecordCreate()
+{
 
-void BFSRecordPrint(struct BFSRecord Record) {
-    // PAListPerformPrint(Record.d);
-    // PACountPerformPrint(Record.n);
 }
+// void BFSRecordDestroy();
+DllExport int BFSRecordFinish(struct BFSRecord*)
+{
+
+}
+DllExport struct BFSRecord BFSRecordDelete(struct BFSRecord*)
+{
+
+}
+DllExport void BFSRecordPrint(struct BFSRecord*)
+{
+
+}
+DllExport struct BFSRecord BFSRecordBegin(struct BFSRecord*, struct PAList* d, struct PACount* n)
+{
+
+}
+DllExport struct BFSRecord BFSRecordCopy(struct BFSRecord* from, struct BFSRecord* to)
+{
+    
+}
+// void BFSRecordPrint(struct BFSRecord Record) {
+//     // PAListPerformPrint(Record.d);
+//     // PACountPerformPrint(Record.n);
+// }
